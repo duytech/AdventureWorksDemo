dd011cf76279c74e2ca5439f5856d22afea9cfe8
Duy <samanosuke146@gmail.com>
2016-08-16 23:28:55 +0700

    Add automapper to model project.

---
+++ MyAdventureWorksDemo/AW.Models/Mapper/AWMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace AW.Models.Mapper
+{
+    using AutoMapper;
+    public class AWMapperProfile : Profile
+    {
+        public AWMapperProfile()
+        {
+            this.CreateMap<DataAccess.Entities.Person, Person>();
+        }
+    }
+}
