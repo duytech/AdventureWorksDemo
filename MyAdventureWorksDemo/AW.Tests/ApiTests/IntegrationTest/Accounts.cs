a543bd9c2089d0434584ca9caf7bbae5ea90609e
Duy <samanosuke146@gmail.com>
2016-09-11 22:24:18 +0700

    Add Test and Web project.

---
+++ MyAdventureWorksDemo/AW.Tests/ApiTests/IntegrationTest/Accounts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AW.Tests.ApiTests.IntegrationTest
+{
+    [TestClass]
+    public class Accounts
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MyAdventureWorksDemo/AW.Tests/ApiTests/UnitTest/Accounts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AW.Tests.ApiTests.UnitTest
+{
+    [TestClass]
+    public class Accounts
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
